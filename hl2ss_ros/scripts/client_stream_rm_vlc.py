8b841710e4a788a2041591dd6082ee06f94e6510
frank-Regal <fregal@utexas.edu>
2025-03-11 19:39:46 -0500

    hololens image streaming script clean up

--- hl2ss_ros/scripts/client_stream_rm_vlc.py
+++ hl2ss_ros/scripts/client_stream_rm_vlc.py
@@ -25,107 +25,110 @@ from viewer import hl2ss_lnm
 from enum import Enum
 
 class HoloLensVLCNode:
+    # -----------------------------------------------------------------------------
+    # Initialize Node
+    # -----------------------------------------------------------------------------
     def __init__(self):
         rospy.init_node('hololens_vlc_node', anonymous=True)
-        self.ag_n = rospy.get_param('~ag_n', '0')
 
         # ROS Parameters
-        self.host = rospy.get_param('~host', '192.168.11.33')
-        self.port = rospy.get_param('~port', hl2ss.StreamPort.RM_VLC_LEFTFRONT)
-        self.mode = rospy.get_param('~mode', hl2ss.StreamMode.MODE_1)
-        self.divisor = rospy.get_param('~divisor', 1)
-        self.profile = rospy.get_param('~profile', hl2ss.VideoProfile.H264_MAIN)
-        self.bitrate = rospy.get_param('~bitrate', None)
+        self.ag_n =       rospy.get_param('~ag_n'      , '0'                               )
+        self.base_frame = rospy.get_param('~base_frame', 'world'                           )
+        self.host =       rospy.get_param('~host'      , '192.168.11.33'                   )
+        self.port =       rospy.get_param('~port'      , hl2ss.StreamPort.RM_VLC_RIGHTRIGHT)
+        self.mode =       rospy.get_param('~mode'      , hl2ss.StreamMode.MODE_1           )
+        self.divisor =    rospy.get_param('~divisor'   , 1                                 )
+        self.profile =    rospy.get_param('~profile'   , hl2ss.VideoProfile.H264_MAIN      )
+        self.bitrate =    rospy.get_param('~bitrate'   , None                              )
 
         # Publishers
         self.image_pub = rospy.Publisher(f'hololens_ag{self.ag_n}/vlc_image', Image, queue_size=10)
-        # self.pose_pub = rospy.Publisher('hololens/vlc_pose', PoseStamped, queue_size=10)
-        self.tf_pub = rospy.Publisher(f'/tf', TFMessage, queue_size=10)
+        self.tf_pub =    rospy.Publisher(f'/tf', TFMessage, queue_size=10)
 
+        # Init class variables
         self.tf_msg = TFMessage()
-        rf_extrinsics = np.array([[-0.998577,    0.05308131, -0.00513458,  0.        ],
-                                  [-0.05296633, -0.9983878,  -0.0204043,   0.        ],
-                                  [-0.00620939, -0.02010331,  0.9997786,   0.        ],
-                                  [-0.00279547, -0.09953252, -0.00105577,  1.        ]])
-        rr_extrinsics = np.array([[ 0.9958912,   0.08903051,  0.01656139,  0.        ],
-                                  [-0.03229338,  0.520009,   -0.85355014,  0.        ],
-                                  [-0.08460408,  0.8495082,   0.5207474,   0.        ],
-                                  [-0.0076595,   0.06369451, -0.08886004,  1.        ]])
-        self.camera_to_rignode = np.linalg.inv(rf_extrinsics.transpose())
         self.bridge = CvBridge()
+        self.child_frame_id = f'{hl2ss.get_port_name(self.port)}'
         self.client = None
         self.log_info = False
 
+    # -----------------------------------------------------------------------------
+    # Start Stream
+    # -----------------------------------------------------------------------------
     def start(self):
+        # Initialize client
         self.client = hl2ss_lnm.rx_rm_vlc(self.host, self.port, mode=self.mode, divisor=self.divisor, profile=self.profile)
         self.client.open()
 
+        # Start processing frames
         rate = rospy.Rate(30)  # Assuming 30 FPS, adjust if needed
         while not rospy.is_shutdown():
             self.process_frame()
             rate.sleep()
 
+        # Close client
         self.client.close()
 
+    # -----------------------------------------------------------------------------
+    # Process Frame
+    # -----------------------------------------------------------------------------
     def process_frame(self):
         data = self.client.get_next_packet()
 
-        # Publish image
-        # Rotate the image 90 degrees clockwise
+        # Manipulate image
         rotated_image = cv2.rotate(data.payload.image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        #rotated_image = data.payload.image
+
+        # Publish image
         img_msg = self.bridge.cv2_to_imgmsg(rotated_image, encoding="mono8")
         img_msg.header.stamp = rospy.Time.now()
         self.image_pub.publish(img_msg)
 
-        # Publish pose if available
+        # Publish /tf if desired
         if self.mode == hl2ss.StreamMode.MODE_1:
 
             # Convert data.pose to pose_msg.pose
-            camera_to_world = data.pose.transpose() # transpose to convert to column-major order
-            camera_to_world_t, camera_to_world_r = self.convert_to_ros(camera_to_world)
-            self.append_tf_msg(camera_to_world_t, camera_to_world_r, 'world', f'{hl2ss.get_port_name(self.port)}', rospy.Time.now())
+            rignode_to_world = data.pose.transpose() # transpose to convert to column-major order
+            rignode_to_world_t, rignode_to_world_r = self.convert_to_ros(rignode_to_world)
 
+            # Append to tf_msg
+            self.append_tf_msg(rignode_to_world_t,
+                               rignode_to_world_r,
+                               self.base_frame,
+                               self.child_frame_id,
+                               rospy.Time.now())
+
+            # Publish tf_msg
             self.tf_pub.publish(self.tf_msg)
             self.tf_msg.transforms.clear()
 
+        # Log info if configured
         if self.log_info == False:
             rospy.loginfo(f'Publishing VLC stream to topic "hololens_ag{self.ag_n}/vlc_image" ...')
             self.log_info = True
-        # rospy.loginfo(f'Sensor Ticks: {data.payload.sensor_ticks}')
-        # rospy.loginfo(f'Exposure: {data.payload.exposure}')
-        # rospy.loginfo(f'Gain: {data.payload.gain}')
 
-    def convert_to_ros(self, transform, rotate=True):
-        # - handle translation
+    # -----------------------------------------------------------------------------
+    # Convert HL2SS transform to ROS transform
+    # -----------------------------------------------------------------------------
+    def convert_to_ros(self, transform):
+        # Handle translation
         translation = self.hl2ss_to_ros_translation(transform[:3, 3]) # convert from HL2SS to ROS convention
-        # - handle rotation
+
+        # Handle rotation
         rotation = self.hl2ss_to_ros_rotation(transform[:3, :3]) # make quaternion and convert to ROS convention
-        if rotate:
-            rotation = self.rotate_about_axis(rotation, -90, axis='x', frame='body')
-            rotation = self.rotate_about_axis(rotation, 180, axis='z', frame='body')
-        r = np.eye(4)
-        r[:3, :3] = rotation
-        orientation = quaternion_from_matrix(r)
-        return translation, orientation
 
-    def convert_to_extrinsics_ros(self, transform):
-
-        rotation = transform[:3, :3]
-        rotation = self.rotate_about_axis(rotation, 180, axis='x', frame='body')
-        # - handle translation
-        translation = self.hl2ss_to_ros_translation(transform[:3, 3]) # convert from HL2SS to ROS convention
-        # - handle rotation
-        rotation = self.hl2ss_to_ros_rotation(rotation) # make quaternion and convert to ROS convention
+        # Rotate about axis
         rotation = self.rotate_about_axis(rotation, -90, axis='x', frame='body')
         rotation = self.rotate_about_axis(rotation, 180, axis='z', frame='body')
 
-
+        # Create 4x4 matrix
         r = np.eye(4)
         r[:3, :3] = rotation
         orientation = quaternion_from_matrix(r)
         return translation, orientation
 
+    # -----------------------------------------------------------------------------
+    # Append to tf_msg
+    # -----------------------------------------------------------------------------
     def append_tf_msg(self, translation, orientation, frame_id, child_frame_id, timestamp=None):
         tf_stamped = TransformStamped()
         tf_stamped.header.stamp = timestamp
@@ -142,8 +145,8 @@ class HoloLensVLCNode:
 
     # -----------------------------------------------------------------------------
     # Convert HL2SS translation to ROS translation
-    # HL2SS (Right-handed): X=right, Y=up, Z=-forward
-    # ROS (Right-handed):   X=forward, Y=left, Z=up
+    #   HL2SS (Right-handed): X=right, Y=up, Z=-forward
+    #   ROS (Right-handed):   X=forward, Y=left, Z=up
     # -----------------------------------------------------------------------------
     def hl2ss_to_ros_translation(self, translation):
         return [-translation[2], -translation[0], translation[1]]
@@ -189,6 +192,9 @@ class HoloLensVLCNode:
         else:
             raise ValueError(f"Invalid frame: {frame}. Please choose 'space' or 'body'.")
 
+    # -----------------------------------------------------------------------------
+    # Query Calibration
+    # -----------------------------------------------------------------------------
     def query_calibration(self):
         data = hl2ss_lnm.download_calibration_rm_vlc(self.host, self.port)
         rospy.loginfo('Calibration data')
